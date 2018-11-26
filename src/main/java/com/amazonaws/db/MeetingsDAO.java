f439b07fe9f4b2f2c7963b368ca54355bcbec99b
Noble Kalish <noblekalish1220@gmail.com>
2018-11-26 18:38:31 -0500

    DAO Change
    created a generateMeeeting method and a deleteMeeting method

--- src/main/java/com/amazonaws/db/MeetingsDAO.java
+++ src/main/java/com/amazonaws/db/MeetingsDAO.java
@@ -1,8 +1,10 @@
 package com.amazonaws.db;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
 
 import com.amazonaws.model.Meeting;
 
@@ -25,7 +27,7 @@ public class MeetingsDAO {
 			ps.setString(1, secretID);
 			ResultSet resultSet = ps.executeQuery();
 			while (resultSet.next()) {
-				meeting = null;//generateConstant(resultSet);
+				meeting = generateMeeting(resultSet);
 			}
 			resultSet.close();
 			ps.close();
@@ -37,5 +39,27 @@ public class MeetingsDAO {
 			throw new Exception("Failed in gettting constant: " + e.getMessage());
 		}
 	}
-	//TODO create a generateMeeting method after database is createds
+	
+	public boolean deleteMeeting(Meeting meeting) throws Exception {
+		try {
+			PreparedStatement ps = conn.prepareStatement("DELETE FROM meetings WHERE secretID=?;");
+			ps.setString(1, meeting.secretID);
+			int numAffected = ps.executeUpdate();
+			ps.close();
+			
+			return (numAffected == 1);
+		} catch (Exception e) {
+			throw new Exception("Failed to delete constant: " + e.getMessage());
+		}
+	}
+	
+	private Meeting generateMeeting(ResultSet resultSet) throws Exception {
+		String participant = resultSet.getString("participant");
+		String secretID = resultSet.getString("secretID");
+		String meetingID = resultSet.getString("meetingID");
+		int startTime = resultSet.getInt("startTime");
+		LocalDate date = resultSet.getDate("date").toLocalDate();
+		boolean isLocked = resultSet.getBoolean("isLocked");
+		return new Meeting(participant, secretID, meetingID, startTime, date, isLocked);
+	}
 }
