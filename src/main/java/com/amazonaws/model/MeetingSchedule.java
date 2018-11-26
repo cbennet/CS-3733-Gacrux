3f52c75c698ef7e569c96d7267012c76db22ca8d
Noble Kalish <noblekalish1220@gmail.com>
2018-11-26 18:13:58 -0500

    Updated models
    Updated models for the meeting and meeting schedule

--- src/main/java/com/amazonaws/model/MeetingSchedule.java
+++ src/main/java/com/amazonaws/model/MeetingSchedule.java
@@ -1,5 +1,33 @@
 package com.amazonaws.model;
 
-public class MeetingSchedule {
+import java.util.Date;
+import java.util.HashMap;
 
+public class MeetingSchedule {
+	public final String secretID;
+	public final String title;
+	public final String scheduleID;
+	public final HashMap<String, Meeting> meetings;
+	public final Date startDate;
+	public final Date endDate;
+	public final int startHour;
+	public final int endHour;
+	public final int meetingDuration;
+	public final Date creationDate;
+	
+	public MeetingSchedule(
+			String secretID, String title, String scheduleID,
+			HashMap<String, Meeting> meetings, Date startDate, Date endDate,
+			int startHour, int endHour, int meetingDuration, Date creationDate) {
+		this.secretID = secretID;
+		this.title = title;
+		this.scheduleID = scheduleID;
+		this.meetings = meetings;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.startHour = startHour;
+		this.endHour = endHour;
+		this.meetingDuration = meetingDuration;
+		this.creationDate = creationDate;
+	}
 }
