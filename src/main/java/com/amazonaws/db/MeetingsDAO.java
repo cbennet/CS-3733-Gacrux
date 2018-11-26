d5ff6acd48cac3f402e08d1d6f895f3bc60984f7
Noble Kalish <noblekalish1220@gmail.com>
2018-11-26 17:56:42 -0500

    Updated DAO and meeting model

--- src/main/java/com/amazonaws/db/MeetingsDAO.java
+++ src/main/java/com/amazonaws/db/MeetingsDAO.java
@@ -18,7 +18,24 @@ public class MeetingsDAO {
 		}
 	}
 	
-	public Meeting getMeeting() {
-		return null;
+	public Meeting getMeeting(String secretID) throws Exception {
+		try {
+			Meeting meeting = null;
+			PreparedStatement ps = conn.prepareStatement("SELECT * FROM meetings WHERE secretID=?;");
+			ps.setString(1, secretID);
+			ResultSet resultSet = ps.executeQuery();
+			while (resultSet.next()) {
+				meeting = null;//generateConstant(resultSet);
+			}
+			resultSet.close();
+			ps.close();
+			
+			return meeting;
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			throw new Exception("Failed in gettting constant: " + e.getMessage());
+		}
 	}
+	//TODO create a generateMeeting method after database is createds
 }
--- src/main/java/com/amazonaws/model/Meeting.java
+++ src/main/java/com/amazonaws/model/Meeting.java
@@ -1,5 +1,18 @@
 package com.amazonaws.model;
 
-public class Meeting {
+import java.util.Date;
 
+public class Meeting {
+	public String participant = null;
+	public final String secretID;
+	public final String meetingID;
+	public final int startTime;
+	public final Date date;
+	
+	public Meeting(String secretID, String meetingID, int startTime, Date date) {
+		this.secretID = secretID;
+		this.meetingID = meetingID;
+		this.startTime = startTime;
+		this.date = date;
+	}
 }
